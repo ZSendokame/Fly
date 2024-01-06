949420678f30dfd1d0e6fc6601bda12f598317b7
ZSendokame <sendokame@sendokame.netlify.app>
2024-01-06 16:52:02 -0300

    Metadata.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+long_description = open('./README.md')
+
+setup(
+    name='Fly',
+    version='1.0.0',
+    url='https://github.com/ZSendokame/Fly',
+    license='MIT license',
+    author='ZSendokame',
+    description='Web scraping library.',
+    long_description=long_description.read(),
+    long_description_content_type='text/markdown',
+
+    packages=(find_packages(include=['fly']))
+)
